b46bac3789817a4357ee7365fbcc5023c35c7c32
Seu Nome <24206090@aluno.univesp.br>
2026-05-14 10:31:54 -0300

    style: remove import não utilizado e adiciona Dockerfile

--- api/api/src/main/java/com/foodtruck/api/controller/DashboardController.java
+++ api/api/src/main/java/com/foodtruck/api/controller/DashboardController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Controller
 public class DashboardController {
